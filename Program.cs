8f03bd5b021f09f9179c2e07fde7153701ab5e20
Opilane TTHK <opilane@tthk.ee>
2019-12-05 13:38:04 +0200

    matrix2

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[][] matrix =
+            {
+                new int []{1,2,3}, ///row 0
+                new int []{4,5,6}, ///row 1
+                new int []{7,8,9} /// row 2
+            };
+
+            Console.WriteLine(matrix[1][1]);
+
+            ///Main diagonal 1,5,9
+            for (int i =0; i< matrix.Length; i++)
+            {
+                Console.Write(matrix[i][i]);
+            }
+            Console.WriteLine();
+            ////Second diagonal 3,5,7
+            for (int i =0; i < matrix.Length; i++)
+            {
+                Console.Write(matrix[i][matrix.Length - 1 -i]);
+            }
+
+            ////Get diagonals: a) 7,5,3 b) 9,5,1
+            Console.ReadKey();
+
+        }
+    }
+}
